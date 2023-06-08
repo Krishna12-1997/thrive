7370e3f7b73604a0a533faa32f0936dedbdd8977
krishna <krishnagnagpure@gmail.com>
2023-06-08 13:31:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+import Navbar from "./component/Navbar";
+import LandingPage from "./pages/LandingPage";
+import Footer from "./component/Footer";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
